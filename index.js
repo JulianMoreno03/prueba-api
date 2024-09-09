12809f00965a6269abf62b1f3343aec158cb2814
Julian Moreno <juliancastillo204@gmail.com>
2024-09-08 20:09:50 -0600

    01

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const port = 3000;
 
 // Definir la ruta principal "/"
 app.get('/', (req, res) => {
-  res.json({ mensaje: 'Hola Mundo' });
+    res.json({ mensaje: 'Hola Mundo' });
 });
+
+app.get('/prueba', (req, res) => {
+    res.json(
+        { "books": [{ "id": 1, "name": "Los 7 hábitos de la gente altamente efectiva", "author": "Stephen R. Covey", "year": 1989 }, { "id": 2, "name": "Padre rico, padre pobre", "author": "Robert Kiyosaki", "year": 1997 }] }
+    );
+});
+
 
 // Escuchar en el puerto 3000
 app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+    console.log(`Servidor escuchando en http://localhost:${port}`);
 });
